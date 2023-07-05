b868555a190ffdbe6fdff6b3f4780fb8de7462b9
Gunduz <gunduz@Gunduzs-MacBook-Air.local>
2023-07-05 20:29:40 +0400

    template inheritance

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='assets')
+app = Flask(__name__)
 
 users = {
     'user_1' : {
@@ -22,11 +22,41 @@ users = {
 
 colors = ['red', 'green', 'blue']
 
-@app.route("/home/")
+products = {
+    1 : {
+        'id' : 1,
+        'title' : 'Product #1',
+        'price' : 100,
+        'description' : 'Some Description #1',
+        'image': 'p1.avif',
+        'colors': ['blue', 'red'],
+    },
+    2 : {
+        'id' : 2,
+        'title' : 'Product #2',
+        'price' : 200,
+        'description' : 'Some Description #2',
+        'image': 'p2.avif',
+        'colors': ['black', 'red']
+    },
+    3 : {
+        'id' : 3,
+        'title' : 'Product #3',
+        'price' : 300,
+        'description' : 'Some Description #3',
+        'image': 'p3.avif',
+        'colors': ['green', 'white']
+    }
+
+}
+
+@app.route("/")
 def home_page():
-    return render_template('index.html', students = users, colors = colors)
+    return render_template('index.html', product_lists = products)
 
 
-@app.route("/about/")
-def about_page():
-    return "<p>About Page</p>"
+@app.route("/product/<int:id>/")
+def product_detail(id):
+    product = products[id]
+    return render_template('product-detail.html', item = product)
+
