ad438323ddd07e209c13a696153f22f3856014b6
Gunduz <gunduz@Gunduzs-MacBook-Air.local>
2023-09-23 20:06:59 +0400

    django session cookies

--- django_codes/stories/urls.py
+++ django_codes/stories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from stories.views import recipes, recipe_detail
+from stories.views import recipes, recipe_detail, like_post
 
 urlpatterns = [
     path('recipes/', recipes, name = 'recipes'),
+    path('like_post/<int:pk>/', like_post, name = 'like_post'),
     path('recipe/<int:pk>/', recipe_detail, name = 'recipe_detail'),
 ]
--- django_codes/stories/views.py
+++ django_codes/stories/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from stories.models import Recipe, Category, Tag
+from django.contrib import messages
 
 # Create your views here.
 
 def recipes(request):
+    print('Liked Posts: ', request.session.get('liked_posts'))
     recipe = Recipe.objects.all()
     context = {
         'recipe_list' : recipe
@@ -23,3 +25,13 @@ def recipe_detail(request, pk):
         'tags' : tags
     }
     return render(request, 'single.html', context)
+
+# def like_post(request, pk):
+#     request.session['liked_posts'] = request.session.get('liked_posts', ' ') + str(pk) + ' '
+#     messages.add_message(request, messages.SUCCESS, "Liked!")
+#     return render(request, 'recipes.html')
+
+def like_post(request, pk):
+    response = HttpResponse('test')
+    response.set_cookie('liked_posts', request.COOKIES.get('liked_posts', ' ') + str(pk) + ' ')
+    return response
